dc3ec49a8ed96fd578a11dcb6b746af7e4d312cb
Letticonion <97452909+Letticonion@users.noreply.github.com>
2022-12-14 14:29:30 +0530

    Fixed Orientation Bug and Removed extra mixins with TickerProvider

--- lib/home.dart
+++ lib/home.dart
@@ -11,9 +11,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with SingleTickerProviderStateMixin {
-
+class _MyHomePageState extends State<MyHomePage> {
   final pageController = PageController(initialPage: 0);
   final imgPath = [
     'assets/images/clock.png',
@@ -22,9 +20,9 @@ class _MyHomePageState extends State<MyHomePage>
   ];
 
   int imgNo = 0;
-  
+
   Widget myWidget = const ImgContainer(imgPath: 'assets/images/clock.png');
-  
+
   void _incrementText() {
     if (pageController.page == 2) {
       pageController.animateToPage(0,
@@ -37,73 +35,164 @@ class _MyHomePageState extends State<MyHomePage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        onPressed: _incrementText,
-        child: const Icon(
-          Icons.arrow_circle_right_rounded,
-          size: 50,
-          color: Colors.white,
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: AnimatedSwitcher(
-              switchInCurve: Curves.easeIn,
-              switchOutCurve: Curves.easeOut,
-              duration: const Duration(milliseconds: 500),
-              child: myWidget,
+    return SingleChildScrollView(
+      child: SizedBox(
+        height: (MediaQuery.of(context).orientation == Orientation.landscape)
+            ? MediaQuery.of(context).size.height * 1.5
+            : MediaQuery.of(context).size.height,
+        child: Scaffold(
+          backgroundColor: Colors.indigo,
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.indigo,
+            onPressed: _incrementText,
+            child: const Icon(
+              Icons.arrow_circle_right_rounded,
+              size: 50,
+              color: Colors.white,
             ),
           ),
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(50),
+          body: Column(
+            children: [
+              Expanded(
+                child: AnimatedSwitcher(
+                  switchInCurve: Curves.easeIn,
+                  switchOutCurve: Curves.easeOut,
+                  duration: const Duration(milliseconds: 500),
+                  child: myWidget,
                 ),
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25, bottom: 20),
-                    child: SmoothPageIndicator(
-                      controller: pageController,
-                      count: 3,
-                      effect: const JumpingDotEffect(
-                          verticalOffset: 10,
-                          dotColor: Colors.lightBlue,
-                          activeDotColor: Colors.indigoAccent),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(50),
                     ),
                   ),
-                  Expanded(
-                    child: PageView(
-                      onPageChanged: (value) => setState(() {
-                        imgNo = value;
-                        myWidget = ImgContainer(
-                            key: ValueKey(value), imgPath: imgPath[imgNo]);
-                      }),
-                      physics: const ClampingScrollPhysics(),
-                      controller: pageController,
-                      children: const [
-                        TextHolder(
-                            heading: stringHeading_1, bodyText: stringBody_1),
-                        TextHolder(
-                            heading: stringHeading_2, bodyText: stringBody_2),
-                        TextHolder(
-                            heading: stringHeading_3, bodyText: stringBody_3),
-                      ],
-                    ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25, bottom: 20),
+                        child: SmoothPageIndicator(
+                          controller: pageController,
+                          count: 3,
+                          effect: const JumpingDotEffect(
+                              verticalOffset: 10,
+                              dotColor: Colors.lightBlue,
+                              activeDotColor: Colors.indigoAccent),
+                        ),
+                      ),
+                      Expanded(
+                        child: PageView(
+                          onPageChanged: (value) => setState(() {
+                            imgNo = value;
+                            myWidget = ImgContainer(
+                                key: ValueKey(value), imgPath: imgPath[imgNo]);
+                          }),
+                          physics: const ClampingScrollPhysics(),
+                          controller: pageController,
+                          children: const [
+                            TextHolder(
+                                heading: stringHeading_1,
+                                bodyText: stringBody_1),
+                            TextHolder(
+                                heading: stringHeading_2,
+                                bodyText: stringBody_2),
+                            TextHolder(
+                                heading: stringHeading_3,
+                                bodyText: stringBody_3),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return SizedBox(
+  //     width: MediaQuery.of(context).size.width,
+  //     height: MediaQuery.of(context).size.height,
+  //     child: SingleChildScrollView(
+  //       child: Scaffold(
+  //         backgroundColor: Colors.indigo,
+  //         floatingActionButton: FloatingActionButton(
+  //           backgroundColor: Colors.indigo,
+  //           onPressed: _incrementText,
+  //           child: const Icon(
+  //             Icons.arrow_circle_right_rounded,
+  //             size: 50,
+  //             color: Colors.white,
+  //           ),
+  //         ),
+  //         body: Column(
+  //           children: [
+  //             Expanded(
+  //               child: AnimatedSwitcher(
+  //                 switchInCurve: Curves.easeIn,
+  //                 switchOutCurve: Curves.easeOut,
+  //                 duration: const Duration(milliseconds: 500),
+  //                 child: myWidget,
+  //               ),
+  //             ),
+  //             Expanded(
+  //               child: Container(
+  //                 decoration: const BoxDecoration(
+  //                   color: Colors.white,
+  //                   borderRadius: BorderRadius.vertical(
+  //                     top: Radius.circular(50),
+  //                   ),
+  //                 ),
+  //                 child: Column(
+  //                   children: [
+  //                     Padding(
+  //                       padding: const EdgeInsets.only(top: 25, bottom: 20),
+  //                       child: SmoothPageIndicator(
+  //                         controller: pageController,
+  //                         count: 3,
+  //                         effect: const JumpingDotEffect(
+  //                             verticalOffset: 10,
+  //                             dotColor: Colors.lightBlue,
+  //                             activeDotColor: Colors.indigoAccent),
+  //                       ),
+  //                     ),
+  //                     Expanded(
+  //                       child: PageView(
+  //                         onPageChanged: (value) => setState(() {
+  //                           imgNo = value;
+  //                           myWidget = ImgContainer(
+  //                               key: ValueKey(value), imgPath: imgPath[imgNo]);
+  //                         }),
+  //                         physics: const ClampingScrollPhysics(),
+  //                         controller: pageController,
+  //                         children: const [
+  //                           TextHolder(
+  //                               heading: stringHeading_1,
+  //                               bodyText: stringBody_1),
+  //                           TextHolder(
+  //                               heading: stringHeading_2,
+  //                               bodyText: stringBody_2),
+  //                           TextHolder(
+  //                               heading: stringHeading_3,
+  //                               bodyText: stringBody_3),
+  //                         ],
+  //                       ),
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
